7832999f15d0ec7f72340f976b0aac36832fd548
ericchiu <ericchiu@guoshipartners.com>
2024-11-26 16:05:58 +0800

    test

--- isip/src/main.ts
+++ isip/src/main.ts
@@ -1,3 +1,3 @@
-const message = 'Hello, World!'
+const message = 'Hello, World!12345'
 const printMessage = (message: string) => console.log(message)
 printMessage(message)
--- oneplayer/src/main.ts
+++ oneplayer/src/main.ts
@@ -1,3 +1,3 @@
-const message = 'Hello, player!123'
+const message = 'Hello, player!12345'
 const printMessage = (message: string) => console.log(message)
 printMessage(message)
