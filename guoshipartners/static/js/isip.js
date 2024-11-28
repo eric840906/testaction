b30178edb051e14377ee7824b56d4ef590375b6e
eric840906 <eric840906@users.noreply.github.com>
2024-11-28 09:25:34 +0000

    test

--- guoshipartners/static/js/isip.js
+++ guoshipartners/static/js/isip.js
@@ -1,2 +1,2 @@
-/*! isip.js 2024-11-28 5:14:29 PM
-*/const s="Hello, World!123",e=o=>console.log(o);e(s);
+/*! isip.js 2024-11-28 5:25:12 PM
+*/const s="Hello, World!",e=o=>console.log(o);e(s);
--- guoshipartners/static/js/oneplayer.js
+++ guoshipartners/static/js/oneplayer.js
@@ -1,2 +1,2 @@
-/*! oneplayer.js 2024-11-28 5:14:35 PM
-*/const s="Hello, player!123123",o=e=>console.log(e);o(s);
+/*! oneplayer.js 2024-11-28 5:25:19 PM
+*/const s="Hello, player!",o=e=>console.log(e);o(s);
