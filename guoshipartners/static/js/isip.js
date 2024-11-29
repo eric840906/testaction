4791aff0c3724b2904c6261c894a444dcaf27b79
eric840906 <eric840906@users.noreply.github.com>
2024-11-29 06:37:12 +0000

    test

--- guoshipartners/static/js/isip.js
+++ guoshipartners/static/js/isip.js
@@ -1,2 +1,2 @@
-/*! isip.js 2024-11-29 9:32:10 AM
-*/const s="Hello, World!",e=o=>console.log(o);e(s);
+/*! isip.js 2024-11-29 2:36:48 PM
+*/const s="Hello, World!123",e=o=>console.log(o);e(s);
--- guoshipartners/static/js/onead_lib.js
+++ guoshipartners/static/js/onead_lib.js
@@ -1,2 +1,2 @@
-/*! onead_lib.js 2024-11-29 9:14:32 AM
-*/const e="Hello, lib!123",o=s=>console.log(s);o(e);
+/*! onead_lib.js 2024-11-29 2:36:53 PM
+*/const e="Hello, lib!123123",o=s=>console.log(s);o(e);
