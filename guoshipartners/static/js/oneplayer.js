f1f158a9f088b66a22a6e5afb846532e8a48cb4f
eric840906 <eric840906@users.noreply.github.com>
2024-11-29 01:32:26 +0000

    fix message format

--- guoshipartners/static/js/oneplayer.js
+++ guoshipartners/static/js/oneplayer.js
@@ -1,2 +1,2 @@
-/*! oneplayer.js 2024-11-29 9:14:33 AM
-*/const s="Hello, player!123",o=e=>console.log(e);o(s);
+/*! oneplayer.js 2024-11-29 9:32:12 AM
+*/const s="Hello, player!",o=e=>console.log(e);o(s);
