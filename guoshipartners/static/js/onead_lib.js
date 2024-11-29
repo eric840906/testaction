8d5629606fdd11b0a0248e9520b7c0bf4d4cbc81
eric840906 <eric840906@users.noreply.github.com>
2024-11-29 01:14:48 +0000

    test

--- guoshipartners/static/js/onead_lib.js
+++ guoshipartners/static/js/onead_lib.js
@@ -1,2 +1,2 @@
-/*! onead_lib.js 2024-11-29 9:03:36 AM
-*/const e="Hello, lib!",o=s=>console.log(s);o(e);
+/*! onead_lib.js 2024-11-29 9:14:32 AM
+*/const e="Hello, lib!123",o=s=>console.log(s);o(e);
